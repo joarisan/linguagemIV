85278ec0d6c2fbf74fe521daa5de3f5b5b43da8c
joari <joari@10.10.5.125>
2024-05-14 11:56:37 -0400

    exemplo de tabelas

---
+++ Tabela/src/main/java/Bean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author joari
+ */
+@Named(value = "bean")
+@SessionScoped
+public class Bean implements Serializable {
+
+    /**
+     * Creates a new instance of Bean
+     */
+    public Bean() {
+    }
+    
+}
---
+++ Tabela/src/main/java/model/Cliente.java
@@ -0,0 +1,31 @@
+
+package model;
+
+/**
+ *
+ * @author joari
+ */
+public class Cliente {
+    private String nomCli;// nome completo do cliente
+    private String conCli; //contato  podendo ser  telefone fixo ou celular
+
+    public String getNomCli() {
+        return nomCli;
+    }
+
+    public void setNomCli(String nomCli) {
+        this.nomCli = nomCli;
+    }
+
+    public String getConCli() {
+        return conCli;
+    }
+
+    public void setConCli(String conCli) {
+        this.conCli = conCli;
+    }
+            
+    
+    
+}//class
+
